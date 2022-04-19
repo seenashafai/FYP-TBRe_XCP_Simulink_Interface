023722773a390f41cc3afe2863a252f18aeaed29
Seena Shafai <seenas@btinternet.com>
2022-04-19 20:07:19 +0100

    fix auto_update

--- auto_update.m
+++ auto_update.m
@@ -1,11 +1,26 @@
+%Identify all lookup tables
 bl = getfullname(Simulink.findBlocksOfType('TBRe_Mod','Lookup_n-D'))
 
-for i = 1:len
+tableData = get_param(char(bl(1)), 'Table')
+breakpoint = get_param(char(bl(1)), 'BreakPointsForDimension1')
+
+
+
+
+
+
+
+%%Replace numerical parameter values with variable names
+for i = 1:length(bl)
+    %Remove path information from block name
     n = reverse(bl(i));
     n = extractBefore(n, "/");
     n = reverse(n);
+    
 
+    %Set 'Data' parameter to 'Name_Data'
     set_param(char(bl(i)), 'Table', n+"_Data")
+    %Set 'Breakpoint 1' parameter to 'Name_bPoint'
     set_param(char(bl(i)), 'BreakPointsForDimension1', n+"_bPoint")
     
 end
