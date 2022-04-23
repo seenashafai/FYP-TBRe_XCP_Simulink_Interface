906d7e35065996dd7d4f933ae527524a76409c86
Seena Shafai <seenas@btinternet.com>
2022-04-23 17:40:58 +0100

    Release
    Bundle for executable

---
+++ main.m
@@ -0,0 +1,15 @@
+%Main script to configure any Simulink model for Calibration
+
+%update params
+model = gcs;
+auto_update(model)
+
+%build + obtain A2L
+rtwbuild(model)
+
+%Get path of a2l & elf files
+modelStr = convertCharsToStrings(model);
+path = modelStr + '_nsi_rtw/';
+
+%populate ECU addresses
+rtw.asap2SetAddress(path + modelStr + '.a2l',path + modelStr + '_FLASH.elf')
